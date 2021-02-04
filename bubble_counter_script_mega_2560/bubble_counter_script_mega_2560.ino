b8d997dbf906b6137cd7bf55fb7524a340fe44ad
Alex Hall <analytics@alex-hall.co.uk>
2021-02-04 23:34:31 +0000

    added shift register driven counter to be driven from 3 pins on arduino

---
+++ bubble_counter_script_mega_2560/bubble_counter_script_mega_2560.ino
@@ -0,0 +1,120 @@
+//D0-D7 -> a-h
+
+int DUTY_CYCLE = 1;
+int BLANKING_DELAY = 100;
+
+int C1 = 8;
+int C2 = 9;
+int C3 = 10;
+int C4 = 11;
+
+void setup() {
+  DDRA = B11111111;  // set port D as output
+  pinMode(C1, OUTPUT);
+  pinMode(C2, OUTPUT);
+  pinMode(C3, OUTPUT);
+  pinMode(C4, OUTPUT);
+
+  digitalWrite(C1, HIGH);
+  digitalWrite(C2, HIGH);
+  digitalWrite(C3, HIGH);
+  digitalWrite(C4, HIGH);
+
+}
+
+int i=0;
+int interval=20; //interval between digit changes
+int previousMillis = 0;
+int currentMillis;
+
+int digit1;
+int digit2;
+int digit3;
+int digit4;
+
+void loop() {
+  currentMillis = millis();
+  if(currentMillis-previousMillis>interval){
+    i++;
+    previousMillis = currentMillis;
+  }
+
+  
+
+  
+ 
+
+  if(i>=1000){
+    digit1 = (i/1000)%10;
+    digitalWrite(C1, LOW);
+  }
+  displayDigit(digit1);
+  delay(DUTY_CYCLE);
+  digitalWrite(C1, HIGH);
+  delayMicroseconds(BLANKING_DELAY);
+
+  if(i>=100){
+    digit2 = (i/100)%10;
+    digitalWrite(C2, LOW);
+  }
+  displayDigit(digit2);
+  delay(DUTY_CYCLE);
+  digitalWrite(C2, HIGH);
+  delayMicroseconds(BLANKING_DELAY);
+
+  if(i>=10){
+    digit3 = (i/10)%10;
+    digitalWrite(C3, LOW);
+  }
+  displayDigit(digit3);
+  delay(DUTY_CYCLE);
+  digitalWrite(C3, HIGH);
+  delayMicroseconds(BLANKING_DELAY);
+
+  if(i>=0){
+   digit4 = (i)%10;
+   digitalWrite(C4, LOW);
+  }
+  displayDigit(digit4);
+  delay(DUTY_CYCLE);
+  digitalWrite(C4, HIGH);
+  delayMicroseconds(BLANKING_DELAY);
+
+}
+
+void displayDigit(int value){
+
+  if(value==0){
+    PORTA = B00111111;
+  }
+  else if(value ==1){
+    PORTA = B00000110;
+  }
+    else if(value ==2){
+    PORTA = B01011011;
+  }
+    else if(value ==3){
+    PORTA = B01001111;
+  }
+    else if(value ==4){
+    PORTA = B01100110;
+  }
+    else if(value ==5){
+    PORTA = B01101101;
+  }
+    else if(value ==6){
+    PORTA = B01111101;
+  }
+    else if(value ==7){
+    PORTA = B00000111;
+  }
+      else if(value ==8){
+    PORTA = B01111111;
+  }
+      else if(value ==9){
+    PORTA = B01101111;
+  }
+  else{
+    PORTA=B00000000;
+  }
+}
---
+++ bubble_counter_script_mega_2560/shift_register/shift_register.ino
@@ -0,0 +1,149 @@
+#include <ShiftRegister74HC595.h>
+
+//control hp led with shift register
+
+
+int numberOfShiftRegisters = 2; // number of shift registers attached in series
+int latchPin = 1; //ST_CP pin
+int clockPin = 0; //SH_CP pin
+int dataPin = 2; //DS pin
+
+int DUTY_CYCLE = 1;
+int BLANKING_DELAY = 100;
+
+ShiftRegister74HC595<2> sr(dataPin, clockPin, latchPin);
+
+byte digit;
+byte digitSelect;
+byte digitArray[10]; //array of segment states
+byte selectArray[4]; //array of digit onoff states
+
+void setup() {
+
+  //set pins to output because they are addressed in the main loop
+
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  //define arrays
+
+  digitArray[0] = 0x3F; //B00111111
+  digitArray[1] = 0x6; //B00000110
+  digitArray[2] = 0x5B; //B01011011
+  digitArray[3] = 0x4F; //0B01001111
+  digitArray[4] = 0x66; //0B01100110
+  digitArray[5] = 0x6D; //0B01101101
+  digitArray[6] = 0x7D; //0B01111101
+  digitArray[7] = 0x7; //0B00000111
+  digitArray[8] = 0x7F; //0B01111111
+  digitArray[9] = 0x6F; //0B01101111
+
+  selectArray[3] = 0x7F; //0B01111111
+  selectArray[2] = 0xBF; //0B10111111
+  selectArray[1] = 0xDF; //0B11011111
+  selectArray[0] = 0xEF; //0B11101111
+
+  sr.setAllLow(); // set all pins LOW
+
+}
+
+int i=1;
+
+void loop(){
+
+  //uint8_t srData[] = {selectArray[2], digitArray[7]};
+  //sr.setAll(srData);
+  displayNumber(i);
+
+  i++;
+}
+
+
+void displayNumber(int num){
+  
+  int digit1;
+  int digit2;
+  int digit3;
+  int digit4;
+
+  uint8_t srData[2];
+
+  if(num >= 1000){
+    digit1 = (num/1000)%10;
+    srData[0] = selectArray[0];
+    srData[1] = digitArray[digit1];
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+  if(num >= 100){
+    digit2 = (num/100)%10;
+    srData[0] = selectArray[1];
+    srData[1] = digitArray[digit2];
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+
+  if(num >= 10){
+    digit3 = (num/10)%10;
+    srData[0] = selectArray[2];
+    srData[1] = digitArray[digit3];
+
+  }
+  else{
+   srData[0] = 0b11111111;
+   srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+
+  if(num >= 1){
+    digit4 = (num)%10;
+    srData[0] = selectArray[3];
+    srData[1] = digitArray[digit4];
+
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+  
+  
+}
