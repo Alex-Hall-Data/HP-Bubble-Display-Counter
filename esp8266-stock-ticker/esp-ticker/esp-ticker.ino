7960c51f3d904f3137a704d246c73260d9f7d8bb
Alex Hall <analytics@alex-hall.co.uk>
2021-02-10 22:55:01 +0000

    added wrapper for coindesk API

--- esp8266-stock-ticker/esp-ticker/esp-ticker.ino
+++ esp8266-stock-ticker/esp-ticker/esp-ticker.ino
@@ -1,10 +1,47 @@
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ShiftRegister74HC595.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#include <WiFiClientSecure.h>
+#include <CoinMarketCapApi.h>
+
+
+
+int numberOfShiftRegisters = 2; // number of shift registers attached in series
+int latchPin = 1; //ST_CP pin
+int clockPin = 0; //SH_CP pin
+int dataPin = 2; //DS pin
+
+int DUTY_CYCLE = 1;
+int BLANKING_DELAY = 100;
+
+ShiftRegister74HC595<2> sr(dataPin, clockPin, latchPin);
+
+byte digit;
+byte digitSelect;
+byte digitArray[10]; //array of segment states
+byte selectArray[4]; //array of digit onoff states
+
+
+#define CD_API "/v1/bpi/currentprice.json"
+#define CD_URL "api.coindesk.com"
+
+WiFiClient client;
+
+int price;
+unsigned long interval = 100000; //ms interval between API queries
+
+// API server
+const char* host = "api.coindesk.com";
 
 void setup() {
+
+    //Serial.begin(9600);
+    //Serial.println("test");
+
+    
     WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
     
     // WiFi.mode(WiFi_STA); // it is a good practice to make sure your code sets wifi mode how you want it.
 
@@ -22,19 +59,229 @@ void setup() {
     bool res;
 
     res = wm.autoConnect("STONKS","onlygoup"); // password protected ap
+    //Serial.println("WiFi connected");  
 
-    if(!res) {
-        Serial.println("Failed to connect");
-        // ESP.restart();
-    } 
-    else {
-        //if you get here you have connected to the WiFi    
-        Serial.println("connected...yeey :)");
-    }
+    //TODO - add error on display if not connected
+
+    // GPIO 1 (TX) swap the pin to a GPIO. - TODO -swap back if serial needed
+  pinMode(1, FUNCTION_3); 
+
+  //set pins to output because they are addressed in the main loop
+
+  pinMode(latchPin, OUTPUT); //TODO - uncomment if serial needed
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  //define arrays
+
+  digitArray[0] = 0x3F; //B00111111
+  digitArray[1] = 0x6; //B00000110
+  digitArray[2] = 0x5B; //B01011011
+  digitArray[3] = 0x4F; //0B01001111
+  digitArray[4] = 0x66; //0B01100110
+  digitArray[5] = 0x6D; //0B01101101
+  digitArray[6] = 0x7D; //0B01111101
+  digitArray[7] = 0x7; //0B00000111
+  digitArray[8] = 0x7F; //0B01111111
+  digitArray[9] = 0x6F; //0B01101111
+
+  selectArray[3] = 0x7F; //0B01111111
+  selectArray[2] = 0xBF; //0B10111111
+  selectArray[1] = 0xDF; //0B11011111
+  selectArray[0] = 0xEF; //0B11101111
+
+  sr.setAllLow(); // set all pins LOW
+
+  price = getData();
 
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-    
+
+//int i=1;
+
+unsigned long previousMillis = 0;
+
+void loop(){
+
+  //displayNumber(i);
+  //i++;
+  
+  displayNumber(price);
+
+  unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+    price = getData();
+
+    }
+  
+}
+
+
+void displayNumber(int num){
+  
+  int digit1;
+  int digit2;
+  int digit3;
+  int digit4;
+
+  uint8_t srData[2];
+
+  if(num >= 1000){
+    digit1 = (num/1000)%10;
+    srData[0] = selectArray[0];
+    srData[1] = digitArray[digit1];
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+  if(num >= 100){
+    digit2 = (num/100)%10;
+    srData[0] = selectArray[1];
+    srData[1] = digitArray[digit2];
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+
+  if(num >= 10){
+    digit3 = (num/10)%10;
+    srData[0] = selectArray[2];
+    srData[1] = digitArray[digit3];
+
+  }
+  else{
+   srData[0] = 0b11111111;
+   srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+
+
+  if(num >= 1){
+    digit4 = (num)%10;
+    srData[0] = selectArray[3];
+    srData[1] = digitArray[digit4];
+
+  }
+  else{
+    srData[0] = 0b11111111;
+    srData[1] = 0b00000000;
+  }
+  sr.setAll(srData);
+
+  delay(DUTY_CYCLE);
+  srData[0] = 0b11111111;
+  srData[1] = 0b00000000;
+  sr.setAll(srData);
+  delayMicroseconds(BLANKING_DELAY);
+
+}
+
+
+
+int getData(){
+
+
+   // Connect to API
+  //Serial.print("connecting to ");
+  //Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    //Serial.println("connection failed");
+
+  }
+  
+  // We now create a URI for the request
+  String url = "/v1/bpi/currentprice.json";
+  
+  //Serial.print("Requesting URL: ");
+  //Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(100);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  String answer;
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    answer += line;
+  }
+
+  client.stop();
+  //Serial.println();
+  //Serial.println("closing connection");
+
+  // Process answer
+  //Serial.println();
+  //Serial.println("Answer: ");
+  //Serial.println(answer);
+
+  // Convert to JSON
+  String jsonAnswer;
+  int jsonIndex;
+
+  for (int i = 0; i < answer.length(); i++) {
+    if (answer[i] == '{') {
+      jsonIndex = i;
+      break;
+    }
+  }
+
+  // Get JSON data
+  jsonAnswer = answer.substring(jsonIndex);
+  //Serial.println();
+  //Serial.println("JSON answer: ");
+  //Serial.println(jsonAnswer);
+  jsonAnswer.trim();
+
+  // Get rate as float
+  int rateIndex = jsonAnswer.indexOf("rate_float");
+  String priceString = jsonAnswer.substring(rateIndex + 12, rateIndex + 18);
+  priceString.trim();
+  float price = priceString.toFloat();
+
+  // Print price
+  //Serial.println();
+  //Serial.println("Bitcoin price: ");
+  //Serial.println(price);
+  int out = round(price);
+  //Serial.println(out);
+  return out;
+
+  
 }
